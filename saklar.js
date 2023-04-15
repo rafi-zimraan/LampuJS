55897d6fb18dc7bffa50854e038fb65788e382ea
rafi-zimraan <raffizimraan27@gmail.com>
2023-04-15 07:43:41 +0700

    deploy

--- saklar.js
+++ saklar.js
@@ -1,39 +1,28 @@
 function saklar(action, inbox) {
-    let toggle = document.getElementById("default-toggle");  
-
-    if (toggle.Checked) {
-        console.log('Di nyalakan');
-    }else {
-        console.log('Di matikan');
-    }
-
-    // console.log('tessss', toggle.Checked);
-
+    let toggle1 = document.getElementById("default-toggle1");
+    let toggle2 = document.getElementById("default-toggle2");  
+    let toggle3 = document.getElementById("default-toggle3");  
+  
     let lampu1 = document.getElementById("lampu1");
     let lampu2 = document.getElementById("lampu2");
     let lampu3 = document.getElementById("lampu3");
 
+    if (toggle1.checked) {
+        lampu1.src = "./images/On.avif"
+    } else {
+        lampu1.src = "./images/Off.avif"
+    }
 
-    if (action == "on") {
-        if (inbox == 1 ) {
-            lampu1.src = "images/On.avif";
-        }
-        if (inbox == 2 ) {
-            lampu2.src = "images/On.avif";
-        }
-        if (inbox == 3 ) {
-            lampu3.src = "images/On.avif";
-        }
+    if (toggle2.checked) {
+        lampu2.src = "./images/On.avif"
+    } else {
+        lampu2.src = "./images/Off.avif"
     }
-    if (action == "off") {
-        if (inbox == 1 ) {
-            lampu1.src = "images/off.avif";
-        }
-        if (inbox == 2 ) {
-            lampu2.src = "images/off.avif";
-        }
-        if (inbox == 3 ) {
-            lampu3.src = "images/off.avif";
-        }
+
+    if (toggle3.checked) {
+        lampu3.src = "./images/On.avif"
+    } else {
+        lampu3.src = "./images/Off.avif"
     }
-}
+    
+} 
